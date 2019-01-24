0e05440f5c6e28563ab815c9977e569fc0d473c9
zz <xzrunner@gmail.com>
2019-01-24 16:12:35 +0800

    [ADDED] clone bp node

--- source/CompNode.cpp
+++ source/CompNode.cpp
@@ -1,6 +1,7 @@
 #include "blueprint/CompNode.h"
+#include "blueprint/Node.h"
 
-#include <assert.h>
+#include <js/RTTR.h>
 
 namespace bp
 {
@@ -10,8 +11,30 @@ const char* const CompNode::TYPE_NAME = "bp_node";
 std::unique_ptr<n0::NodeComp> CompNode::Clone(const n0::SceneNode& node) const
 {
 	auto comp = std::make_unique<CompNode>();
-	// todo
-	assert(0);
+    if (!m_node) {
+        return comp;
+    }
+
+    auto type = m_node->get_type();
+    auto ctor = type.get_constructor();
+    auto var = ctor.invoke();
+    auto dst = var.get_value<std::shared_ptr<Node>>();
+    comp->SetNode(dst);
+
+    for (auto& prop : type.get_properties())
+    {
+        if (prop.get_metadata(js::RTTR::NoSerializeTag())) {
+            continue;
+        }
+
+        rttr::variant prop_value = prop.get_value(m_node);
+        if (!prop_value) {
+            continue;
+        }
+
+        prop.set_value(dst, prop_value);
+    }
+
 	return comp;
 }
 
