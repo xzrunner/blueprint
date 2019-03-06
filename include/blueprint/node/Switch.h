21d0f9589729cec59d2746099ccabe58002e4893
zz <xzrunner@gmail.com>
2019-03-06 15:04:04 +0800

    [CHANGED] move from prop to input

--- include/blueprint/node/Switch.h
+++ include/blueprint/node/Switch.h
@@ -13,26 +13,22 @@ class Switch : public Node
 public:
     Switch()
         : Node("Switch")
-        , m_toggle(true)
     {
-        AddPins(std::make_shared<Pins>(true,  0, PINS_ANY_VAR, "True", *this));
-        AddPins(std::make_shared<Pins>(true,  1, PINS_ANY_VAR, "False", *this));
+        AddPins(std::make_shared<Pins>(true, 0, PINS_ANY_VAR, "True", *this));
+        AddPins(std::make_shared<Pins>(true, 1, PINS_ANY_VAR, "False", *this));
+        AddPins(std::make_shared<Pins>(true, 2, PINS_BOOLEAN, "Value", *this));
+
         AddPins(std::make_shared<Pins>(false, 0, PINS_ANY_VAR, "Out", *this));
 
         Layout();
     }
 
-    bool GetToggle() const { return m_toggle; }
-    void SetToggle(bool toggle) { m_toggle = toggle; }
-
     enum InputID
     {
         ID_TRUE = 0,
-        ID_FALSE
+        ID_FALSE,
+        ID_VALUE,
     };
-
-private:
-    bool m_toggle;
 
     RTTR_ENABLE(Node)
 
