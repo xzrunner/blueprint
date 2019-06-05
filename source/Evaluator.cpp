c287653d33ae657a39632013df71853083cf9be1
zz <zhuguang@ejoy.com>
2019-06-05 08:03:13 +0800

    [ADDED] for index

--- source/Evaluator.cpp
+++ source/Evaluator.cpp
@@ -11,11 +11,12 @@
 #include "blueprint/node/Divide.h"
 #include "blueprint/node/Combine.h"
 #include "blueprint/node/Split.h"
+#include "blueprint/node/For.h"
 
 namespace bp
 {
 
-float Evaluator::CalcFloat(const Connecting& conn)
+float Evaluator::CalcFloat(const Connecting& conn, int for_idx)
 {
     float ret = 0;
 
@@ -28,53 +29,63 @@ float Evaluator::CalcFloat(const Connecting& conn)
     else if (node_type == rttr::type::get<node::Negate>())
     {
         auto& conns = node.GetAllInput()[0]->GetConnecting();
-        ret = conns.empty() ? 0 : CalcFloat(*conns[0]);
+        ret = conns.empty() ? 0 : -CalcFloat(*conns[0], for_idx);
     }
     else if (node_type == rttr::type::get<node::Add>())
     {
         auto& conns_a = node.GetAllInput()[node::Add::ID_A]->GetConnecting();
-        float a = conns_a.empty() ? 0 : CalcFloat(*conns_a[0]);
+        float a = conns_a.empty() ? 0 : CalcFloat(*conns_a[0], for_idx);
 
         auto& conns_b = node.GetAllInput()[node::Add::ID_B]->GetConnecting();
-        float b = conns_b.empty() ? 0 : CalcFloat(*conns_b[0]);
+        float b = conns_b.empty() ? 0 : CalcFloat(*conns_b[0], for_idx);
 
         ret = a + b;
     }
     else if (node_type == rttr::type::get<node::Subtract>())
     {
         auto& conns_a = node.GetAllInput()[node::Subtract::ID_A]->GetConnecting();
-        float a = conns_a.empty() ? 0 : CalcFloat(*conns_a[0]);
+        float a = conns_a.empty() ? 0 : CalcFloat(*conns_a[0], for_idx);
 
         auto& conns_b = node.GetAllInput()[node::Subtract::ID_B]->GetConnecting();
-        float b = conns_b.empty() ? 0 : CalcFloat(*conns_b[0]);
+        float b = conns_b.empty() ? 0 : CalcFloat(*conns_b[0], for_idx);
 
         ret = a - b;
     }
     else if (node_type == rttr::type::get<node::Multiply>())
     {
         auto& conns_a = node.GetAllInput()[node::Multiply::ID_A]->GetConnecting();
-        float a = conns_a.empty() ? 0 : CalcFloat(*conns_a[0]);
+        float a = conns_a.empty() ? 0 : CalcFloat(*conns_a[0], for_idx);
 
         auto& conns_b = node.GetAllInput()[node::Multiply::ID_B]->GetConnecting();
-        float b = conns_b.empty() ? 0 : CalcFloat(*conns_b[0]);
+        float b = conns_b.empty() ? 0 : CalcFloat(*conns_b[0], for_idx);
 
-        ret = a + b;
+        ret = a * b;
     }
     else if (node_type == rttr::type::get<node::Divide>())
     {
         auto& conns_a = node.GetAllInput()[node::Divide::ID_A]->GetConnecting();
-        float a = conns_a.empty() ? 0 : CalcFloat(*conns_a[0]);
+        float a = conns_a.empty() ? 0 : CalcFloat(*conns_a[0], for_idx);
 
         auto& conns_b = node.GetAllInput()[node::Divide::ID_B]->GetConnecting();
-        float b = conns_b.empty() ? 0 : CalcFloat(*conns_b[0]);
+        float b = conns_b.empty() ? 0 : CalcFloat(*conns_b[0], for_idx);
 
-        ret = a + b;
+        ret = a / b;
+    }
+    else if (node_type == rttr::type::get<node::For>())
+    {
+        ret = static_cast<float>(for_idx);
+    }
+    else
+    {
+        auto type = node_type.get_name().to_string();
+        printf("unknown type %s\n", type.c_str());
+        assert(0);
     }
 
     return ret;
 }
 
-sm::vec3 Evaluator::CalcFloat3(const Connecting& conn)
+sm::vec3 Evaluator::CalcFloat3(const Connecting& conn, int for_idx)
 {
     sm::vec3 ret;
 
@@ -87,60 +98,46 @@ sm::vec3 Evaluator::CalcFloat3(const Connecting& conn)
     else if (node_type == rttr::type::get<node::Negate>())
     {
         auto& conns = node.GetAllInput()[0]->GetConnecting();
-        ret = conns.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns[0]);
+        ret = conns.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns[0], for_idx);
     }
     else if (node_type == rttr::type::get<node::Combine>())
     {
         auto& conns_r = node.GetAllInput()[node::Combine::ID_R]->GetConnecting();
-        float r = conns_r.empty() ? 0 : CalcFloat(*conns_r[0]);
+        float r = conns_r.empty() ? 0 : CalcFloat(*conns_r[0], for_idx);
 
         auto& conns_g = node.GetAllInput()[node::Combine::ID_G]->GetConnecting();
-        float g = conns_g.empty() ? 0 : CalcFloat(*conns_g[0]);
+        float g = conns_g.empty() ? 0 : CalcFloat(*conns_g[0], for_idx);
 
         auto& conns_b = node.GetAllInput()[node::Combine::ID_B]->GetConnecting();
-        float b = conns_b.empty() ? 0 : CalcFloat(*conns_b[0]);
+        float b = conns_b.empty() ? 0 : CalcFloat(*conns_b[0], for_idx);
 
         ret = sm::vec3(r, g, b);
     }
     else if (node_type == rttr::type::get<node::Add>())
     {
         auto& conns_a = node.GetAllInput()[node::Add::ID_A]->GetConnecting();
-        sm::vec3 a = conns_a.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_a[0]);
+        sm::vec3 a = conns_a.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_a[0], for_idx);
 
         auto& conns_b = node.GetAllInput()[node::Add::ID_B]->GetConnecting();
-        sm::vec3 b = conns_b.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_b[0]);
+        sm::vec3 b = conns_b.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_b[0], for_idx);
 
         ret = a + b;
     }
     else if (node_type == rttr::type::get<node::Subtract>())
     {
         auto& conns_a = node.GetAllInput()[node::Subtract::ID_A]->GetConnecting();
-        sm::vec3 a = conns_a.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_a[0]);
+        sm::vec3 a = conns_a.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_a[0], for_idx);
 
         auto& conns_b = node.GetAllInput()[node::Subtract::ID_B]->GetConnecting();
-        sm::vec3 b = conns_b.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_b[0]);
+        sm::vec3 b = conns_b.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_b[0], for_idx);
 
         ret = a - b;
     }
-    else if (node_type == rttr::type::get<node::Multiply>())
+    else
     {
-        auto& conns_a = node.GetAllInput()[node::Multiply::ID_A]->GetConnecting();
-        sm::vec3 a = conns_a.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_a[0]);
-
-        auto& conns_b = node.GetAllInput()[node::Multiply::ID_B]->GetConnecting();
-        sm::vec3 b = conns_b.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_b[0]);
-
-        ret = a + b;
-    }
-    else if (node_type == rttr::type::get<node::Divide>())
-    {
-        auto& conns_a = node.GetAllInput()[node::Divide::ID_A]->GetConnecting();
-        sm::vec3 a = conns_a.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_a[0]);
-
-        auto& conns_b = node.GetAllInput()[node::Divide::ID_B]->GetConnecting();
-        sm::vec3 b = conns_b.empty() ? sm::vec3(0, 0, 0) : CalcFloat3(*conns_b[0]);
-
-        ret = a + b;
+        auto type = node_type.get_name().to_string();
+        printf("unknown type %s\n", type.c_str());
+        assert(0);
     }
 
     return ret;
