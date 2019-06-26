eecce11e2f6345ce161527ceb6f8646f6ad6f7ff
zz <zhuguang@ejoy.com>
2019-06-27 06:56:23 +0800

    [ADDED] StageFuncNode for StagePage which use the Function node

---
+++ include/blueprint/StageFuncNode.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "blueprint/typedef.h"
+
+#include <node0/typedef.h>
+
+#include <string>
+
+namespace bp
+{
+
+class StageFuncNode
+{
+public:  
+    void SetParentNode(const n0::SceneNodePtr& parent_node) {
+        m_parent_node = parent_node;
+    }
+    void EnableInsert(bool enable) { m_insert_enable = enable; }
+
+    void SetFilepath(const std::string& filepath);
+
+    void InsertSceneObj(const n0::SceneNodePtr& node);
+    void DeleteSceneObj(const n0::SceneNodePtr& node);
+    void ClearSceneObj();
+
+private:
+    void UpdateParentAABB(const NodePtr& node);
+
+private:
+    // bp::node::Function
+    n0::SceneNodePtr m_parent_node = nullptr;
+
+    bool m_insert_enable = true;
+
+}; // StageFuncNode
+
+}
---
+++ source/StageFuncNode.cpp
@@ -0,0 +1,95 @@
+#include "blueprint/StageFuncNode.h"
+#include "blueprint/Node.h"
+#include "blueprint/CompNode.h"
+#include "blueprint/NodeHelper.h"
+#include "blueprint/node/Function.h"
+
+#include <node0/SceneNode.h>
+#include <node2/CompBoundingBox.h>
+
+namespace bp
+{
+
+void StageFuncNode::SetFilepath(const std::string& filepath)
+{
+    if (m_parent_node) {
+        if (m_parent_node->HasUniqueComp<CompNode>()) {
+            auto& cnode = m_parent_node->GetUniqueComp<CompNode>();
+            auto bp_node = cnode.GetNode();
+            if (bp_node->get_type() == rttr::type::get<node::Function>()) {
+                auto func_node = std::static_pointer_cast<node::Function>(bp_node);
+                func_node->SetFilepath(filepath);
+            }
+        }
+    }
+}
+
+void StageFuncNode::UpdateParentAABB(const NodePtr& node)
+{
+    auto& st = node->GetStyle();
+    m_parent_node->GetUniqueComp<n2::CompBoundingBox>().SetSize(
+        *m_parent_node, sm::rect(st.width, st.height)
+    );
+}
+
+void StageFuncNode::InsertSceneObj(const n0::SceneNodePtr& node)
+{
+    // load function nodes
+    auto& cnode = node->GetUniqueComp<bp::CompNode>();
+    auto bp_node = cnode.GetNode();
+    if (bp_node->get_type() == rttr::type::get<bp::node::Function>()) {
+        NodeHelper::LoadFunctionNode(node, bp_node);
+    }
+
+    if (m_insert_enable && m_parent_node)
+    {
+        if (m_parent_node->HasUniqueComp<bp::CompNode>())
+        {
+            auto& cnode = m_parent_node->GetUniqueComp<bp::CompNode>();
+            auto bp_node = cnode.GetNode();
+            if (bp_node->get_type() == rttr::type::get<bp::node::Function>()) {
+                auto func_node = std::static_pointer_cast<bp::node::Function>(bp_node);
+                if (bp::node::Function::AddChild(func_node, node)) {
+                    UpdateParentAABB(func_node);
+                }
+            }
+        }
+    }
+}
+
+void StageFuncNode::DeleteSceneObj(const n0::SceneNodePtr& node)
+{
+    if (m_parent_node)
+    {
+        if (m_parent_node->HasUniqueComp<bp::CompNode>())
+        {
+            auto& cnode = m_parent_node->GetUniqueComp<bp::CompNode>();
+            auto bp_node = cnode.GetNode();
+            if (bp_node->get_type() == rttr::type::get<bp::node::Function>()) {
+                auto func_node = std::static_pointer_cast<bp::node::Function>(bp_node);
+                if (bp::node::Function::RemoveChild(func_node, node)) {
+                    UpdateParentAABB(func_node);
+                }
+            }
+        }
+    }
+}
+
+void StageFuncNode::ClearSceneObj()
+{
+    if (m_parent_node)
+    {
+        if (m_parent_node->HasUniqueComp<bp::CompNode>())
+        {
+            auto& cnode = m_parent_node->GetUniqueComp<bp::CompNode>();
+            auto bp_node = cnode.GetNode();
+            if (bp_node->get_type() == rttr::type::get<bp::node::Function>()) {
+                auto func_node = std::static_pointer_cast<bp::node::Function>(bp_node);
+                func_node->ClearChildren();
+                UpdateParentAABB(func_node);
+            }
+        }
+    }
+}
+
+}
