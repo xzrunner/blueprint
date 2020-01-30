ca1049aec99fcce11e6176f3bf2491e59cf96f17
zz <xzrunner@gmail.com>
2020-01-31 07:49:30 +0800

    [FIXED] send msg with obj

--- source/WxNodeProperty.cpp
+++ source/WxNodeProperty.cpp
@@ -67,7 +67,7 @@ void WxNodeProperty::LoadFromNode(const n0::SceneNodePtr& obj, const NodePtr& no
                     bp::NodeHelper::LoadFunctionNode(m_obj, m_node);
                 }
                 m_sub_mgr->NotifyObservers(ee0::MSG_SET_CANVAS_DIRTY);
-                m_sub_mgr->NotifyObservers(bp::MSG_BP_NODE_PROP_CHANGED);
+                ee0::MsgHelper::SendObjMsg(*m_sub_mgr, m_obj, bp::MSG_BP_NODE_PROP_CHANGED);
             });
         }
 	}
